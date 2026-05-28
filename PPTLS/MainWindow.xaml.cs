6a0978100cd48bab548a278bbd7bebc5fb2c2410
Sanmi47 <camila.smc0407@gmail.com>
2026-05-27 18:47:11 -0700

    se me olvido esto xd

--- PPTLS/MainWindow.xaml.cs
+++ PPTLS/MainWindow.xaml.cs
@@ -1,23 +1,36 @@
-using System.Text;
+﻿using System;
+using System.Media;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using PPTLS.Models;
+using PPTLS.Strategies;
 namespace PPTLS;
+public partial class MainWindow : Window{
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
-{
-    public MainWindow()
-    {
+IJugada Jugada;
+    private Juego juego;
+    public MainWindow(){
         InitializeComponent();
+        juego = new Juego();
+    }
+    private void BtnPiedra_Click(object sender, RoutedEventArgs e){
+        Jugada = new Piedra();
+        MessageBox.Show(juego.Jugar(Jugada));
+    }
+    private void BtnPapel_Click(object sender, RoutedEventArgs e){
+        Jugada = new Papel();
+        MessageBox.Show(juego.Jugar(Jugada));
+    }
+    private void BtnTijera_Click(object sender, RoutedEventArgs e){
+        Jugada = new Tijera();
+        MessageBox.Show(juego.Jugar(Jugada));
+    }
+    private void BtnLagarto_Click(object sender, RoutedEventArgs e){
+        Jugada = new Lagarto();
+        MessageBox.Show(juego.Jugar(Jugada));
+    }
+    private void BtnSpock_Click(object sender, RoutedEventArgs e){
+        Jugada = new Spock();
+        MessageBox.Show(juego.Jugar(Jugada));
     }
 }
